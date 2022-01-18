2dcc5698e458db5deb6f9492816c4d06fa46f257
lipin <lipinclp@gmail.com>
2022-01-18 09:16:18 +0800

    dml sql day 2

---
+++ Day2/Work/DML.sql
@@ -0,0 +1,7 @@
+use Northwind
+
+select * from Products
+
+select ProductName from Products
+select ProductName, QuantityPerUnit from Products
+select ProductName Product_Name, QuantityPerUnit Quantity_In_Every_Unit from Products   --provide an alias name for display purposes
